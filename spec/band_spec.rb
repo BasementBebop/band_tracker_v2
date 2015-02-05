b95504d34a1768ef51423d771fca0f0a28703cfa
Sean Holstrom <basementbebop@gmail.com>
2015-02-05 12:30:29 -0800

    Update spec details

--- spec/band_spec.rb
+++ spec/band_spec.rb
@@ -1,4 +1,15 @@
-require "spec_helper"
-describe(Band) {
-  it { should have_and_belong_to_many(:venues) }
-}
+require("spec_helper")
+
+describe(Band) do
+  it {should have_and_belong_to_many :venues}
+
+  it("validates the presence of a band name") do
+    band = Band.new({:name => ""})
+    expect(band.save).to eq false
+  end
+
+  it("capitalizes letters if titleized name") do
+    band = Band.create({:name => "sean"})
+    expect(band.name).to eq "Sean"
+  end
+end
--- spec/venue_spec.rb
+++ spec/venue_spec.rb
@@ -1,4 +1,15 @@
 require "spec_helper"
-describe(Venue) {
-  it { should have_and_belong_to_many(:bands) }
-}
+
+describe Venue do
+  it {should have_and_belong_to_many :bands}
+
+  it "validates the presence of a venue name" do
+    venue = Venue.new({:name => ""})
+    expect(venue.save).to eq false
+  end
+
+  it "capitalizes letters if titleized name" do
+    venue = Venue.create({:name => "somewhere"})
+    expect(venue.name).to eq "Somewhere"
+  end
+end
